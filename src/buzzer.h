cb342d8d739c06db2b87e7a09a96e5921c3d2282
Chris Heppner <16630599+heepdog@users.noreply.github.com>
2021-04-29 17:17:49 -0500

    weekend commit

--- src/buzzer.h
+++ src/buzzer.h
@@ -1,17 +1,41 @@
 #include "Arduino.h"
 
 class buzzer{
+    const int pinNumber;
+
     public:
-        buzzer(int pin){ invertOut = false; setup(pin);};
-        buzzer(int pin, bool invert){invertOut = invert; setup(pin);}
-        void buzzerOn(int secondsOn, int mode){timeON = millis()+(secondsOn*1000);digitalWrite(pinNumber,!invertOut);on=true;};
-        void update(){ if( on && (timeON < millis()) ){digitalWrite(pinNumber,invertOut);}};
+        buzzer(int pin):pinNumber(pin){
+
+            invertOut = false; setup();
+            };
+        buzzer(int pin, bool invert):pinNumber(pin){
+            invertOut = invert; 
+            setup();
+            };
+        void buzzerOn(int secondsOn, int mode){
+            Serial.printf("\nturning on buzzer for %d Pin: %d \n", + secondsOn, pinNumber);
+            timeON = millis()+(secondsOn*1000);
+            digitalWrite(pinNumber,!invertOut);
+            on=true;
+            };
+        void update(){
+            if( on && (timeON < millis()) ){
+                Serial.printf("\n\nturning off buzzer on pin: %d \n", pinNumber);
+                digitalWrite(pinNumber,invertOut);
+                timeON = 0;
+                on = false;
+            };
+        };
         
         
 
     private:
-        void setup(int pin){pinNumber = pin; pinMode(pinNumber,OUTPUT);digitalWrite(pinNumber,invertOut);}
-        int pinNumber;
+        void setup(){
+            pinMode(pinNumber,OUTPUT);
+            digitalWrite(pinNumber,invertOut);
+            timeON = 0;
+            on = false;
+        };
         bool on;
         unsigned long timeON;
         bool invertOut;
